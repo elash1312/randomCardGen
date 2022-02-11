7864ccc398b179991bea5bc93c7a85b2f38173a3
elash1312 <lasherin1312@gmail.com>
2022-02-11 21:37:25 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ import "./assets/img/4geeks.ico";
 
 window.onload = () => {
   document.querySelector(".card").classList.add("hearts");
+  setTimeout(() => {
+    document.querySelector(".card").classList.remove("hearts");
+    document.querySelector(".card").classList.add("spades");
+  }, 1000);
   document.querySelector(".card").classList.add("spades");
   document.querySelector(".card").classList.add("diamonds");
   document.querySelector(".card").classList.add("clubs");
