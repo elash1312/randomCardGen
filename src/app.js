a04a440e65be1838b7ef9e70e145fbebf5fcd96b
elash1312 <lasherin1312@gmail.com>
2022-04-13 00:27:01 +0000

    reconfigured code, still not working

--- src/app.js
+++ src/app.js
@@ -4,14 +4,33 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = () => {
-  document.querySelector(".card").classList.add("hearts");
-  setTimeout(() => {
-    document.querySelector(".card").classList.remove("hearts");
-    document.querySelector(".card").classList.add("spades");
-  }, 1000);
-  document.querySelector(".card").classList.add("spades");
-  document.querySelector(".card").classList.add("diamonds");
-  document.querySelector(".card").classList.add("clubs");
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
