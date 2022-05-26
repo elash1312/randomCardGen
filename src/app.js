c3888d8cde43bbc93d015ea1b68c0a237d530cd2
elash1312 <lasherin1312@gmail.com>
2022-05-26 05:33:32 +0000

    project rebuilt, implemented innerHTML classes

--- src/app.js
+++ src/app.js
@@ -2,15 +2,8 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-window.onload = () => {
-  document.querySelector(".card").classList.add(generateRandomSuit());
-  document.querySelector(".card").innerHTML = generateRandomNumber();
-};
-
-let generateRandomNumber = () => {
-  let numbers = [
+function deckBuilder() {
+  const values = [
     "A",
     "2",
     "3",
@@ -25,14 +18,42 @@ let generateRandomNumber = () => {
     "Q",
     "K"
   ];
-  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
 
-  return numbers[indexNumbers];
-};
+  const cards = [];
+  for (let s = 0; s < suits.length; s++) {
+    for (let v = 0; v < values.length; v++) {
+      const value = values[v];
+      const suit = suits[s];
+      cards.push({ value, suit });
+    }
+  }
+  return cards;
+}
 
-let generateRandomSuit = () => {
-  let suit = ["diamonds", "spades", "hearts", "clubs"];
-  let indexSuit = Math.floor(Math.random() * suit.length);
+function randomCard(cards) {
+  const random = Math.floor(Math.random() * 51);
+  const cardValue = cards[random].value;
+  const cardSuit = cards[random].suit;
 
-  return suit[indexSuit];
-};
+  const cardBody =
+    cardSuit === "Diamonds" ? "&diams;" : "&" + cardSuit.toLowerCase() + ";";
+
+  const card = document.createElement("div");
+  card.classList.add("card", cardSuit.toLowerCase());
+  card.innerHTML =
+    '<span class="card-value-suit-top">' +
+    cardBody +
+    "</span>" +
+    '<span class="card-value-center">' +
+    cardValue +
+    "</span>" +
+    '<span class="card-value-suit-bot">' +
+    cardBody +
+    "</span>" +
+    "</span>";
+  document.body.appendChild(card);
+}
+
+const cards = deckBuilder();
+randomCard(cards);
